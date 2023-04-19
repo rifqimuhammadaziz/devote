d49890e5a0f426827052aea788c1de165e50ea32
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-19 11:57:27 +0700

    add assets & refactoring core data

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Rifqi Muhammad Aziz on 19/04/23.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
