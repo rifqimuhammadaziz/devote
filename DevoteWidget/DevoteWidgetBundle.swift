e918a7a19439e4b9f2adc095b2c1803eaf613b58
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-06-01 09:30:10 +0700

    create widget app

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Rifqi Muhammad Aziz on 19/05/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
